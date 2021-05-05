c2a716b91e1f96934a4094d8b6202c47e5b937fc
rakify <57259304+rakify@users.noreply.github.com>
2021-05-05 16:10:53 +0600

    Update post.js

--- src/routes/post.js
+++ src/routes/post.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const router = express();
+const router = express.Router();
 router.use(cookieParser());
 const {
     postValidation
